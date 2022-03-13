2508cad565bcde7fdd3c76b6121008183974ac30
rlss91 <rlss91@gmail.com>
2022-03-13 19:24:44 +0200

    string interpulation

---
+++ src/components/BigTextComponent.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const BigTextComponent = () => {
+  const title = "Hello world";
+  return (
+    <Fragment>
+      <h1>{title}</h1>
+      <h2>Another Text</h2>
+    </Fragment>
+  );
+};
+
+export default BigTextComponent;
