12cb781a4fbd6e3bf751523bf0c7ab1b305a0e2a
rlss91 <rlss91@gmail.com>
2022-03-20 20:32:01 +0200

    if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 
 // import BigTextComponent from "./components/BigTextComponent";
-// import LoginComponent from "./components/LoginComponent";
-import RegisterComponent from "./components/RegisterComponent";
+import LoginComponent from "./components/LoginComponent";
+// import RegisterComponent from "./components/RegisterComponent";
 
 function App() {
   return (
     <div className="container">
       {/* <BigTextComponent /> */}
-      {/* <LoginComponent /> */}
-      <RegisterComponent />
+      <LoginComponent />
+      {/* <RegisterComponent /> */}
     </div>
   );
 }
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 const LoginComponent = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
 
   const handleEmailChange = (event) => {
     // console.log(event.target.value);
@@ -12,41 +13,63 @@ const LoginComponent = () => {
     // console.log(event.target.value);
     setPassword(event.target.value);
   };
+  const handleBtnClick = (event) => {
+    event.preventDefault();
+    if (email && password) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
 
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
+    <Fragment>
+      <form onSubmit={handleBtnClick}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
         </div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+      {show && (
+        <div>
+          <p>Email: {email}</p>
+          <p>Password: {password}</p>
+        </div>
+      )}
+      {show ? (
+        <div>
+          <p>Email: {email}</p>
+          <p>Password: {password}</p>
+        </div>
+      ) : (
+        <div>Please fill email and password</div>
+      )}
+    </Fragment>
   );
 };
 
