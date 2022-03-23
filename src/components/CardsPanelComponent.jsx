5cc046c1c81f6976cf87c9d325a8e224f6f479e4
rlss91 <rlss91@gmail.com>
2022-03-23 18:52:48 +0200

    ffc

--- src/components/CardsPanelComponent.jsx
+++ src/components/CardsPanelComponent.jsx
@@ -1,22 +1,36 @@
-const dataArr = [
-  { name: "sadfsdfasdf1", desc: "asdfrehserhsrgeargerherth1" },
-  { name: "sadfsdfasdf2", desc: "asdfrehserhsrgeargerherth2" },
-  { name: "sadfsdfasdf3", desc: "asdfrehserhsrgeargerherth3" },
-  { name: "sadfsdfasdf4", desc: "asdfrehserhsrgeargerherth4" },
+import { useState } from "react";
+import { cloneDeep } from "lodash";
+import CardComponent from "./CardComponent";
+
+const dataArrInitial = [
+  { name: "sadfsdfasdf1", desc: "asdfrehserhsrgeargerherth1", id: 1 },
+  { name: "sadfsdfasdf2", desc: "asdfrehserhsrgeargerherth2", id: 2 },
+  { name: "sadfsdfasdf3", desc: "asdfrehserhsrgeargerherth3", id: 3 },
+  { name: "sadfsdfasdf4", desc: "asdfrehserhsrgeargerherth4", id: 4 },
 ];
 
 const CardsPanelComponent = () => {
+  const [dataArr, setDataArr] = useState(dataArrInitial);
+
+  const handleDeleteCard = (id) => {
+    console.log("from father", id);
+    let newDataArr = cloneDeep(dataArr);
+    newDataArr = newDataArr.filter((item) => {
+      return item.id !== id;
+    });
+    setDataArr(newDataArr);
+  };
   return (
     <div>
       {dataArr.map((item) => {
         return (
-          <div className="card" key={item.name}>
-            <div className="card-body">
-              <h5 className="card-title">{item.name}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text">{item.desc}</p>
-            </div>
-          </div>
+          <CardComponent
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            key={item.id}
+            onDeleteCard={handleDeleteCard}
+          />
         );
       })}
     </div>
@@ -24,3 +38,12 @@ const CardsPanelComponent = () => {
 };
 
 export default CardsPanelComponent;
+{
+  /* <div className="card" key={item.name}>
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">{item.desc}</p>
+            </div>
+          </div> */
+}
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,9 +3,9 @@ const linksArr = ["Home", "Contact", "About us"];
 const NavbarComponent = () => {
   return (
     <ul className="nav">
-      {linksArr.map((item) => {
+      {linksArr.map((item, idx) => {
         return (
-          <li className="nav-item">
+          <li className="nav-item" key={idx}>
             <a className="nav-link" aria-current="page" href="#">
               {item}
             </a>
