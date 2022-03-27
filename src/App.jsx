fbe4816a6cda1538b76fc238f6eb3abf87a3f755
rlss91 <rlss91@gmail.com>
2022-03-27 21:01:59 +0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       {/* <BigTextComponent /> */}
       {/* <LoginComponent /> */}
       {/* <RegisterComponent /> */}
-      {/* <CardsPanelComponent /> */}
-      <TimerWrapperComponent />
+      <CardsPanelComponent />
+      {/* <TimerWrapperComponent /> */}
     </div>
   );
 }
--- src/components/CardsPanelComponent.jsx
+++ src/components/CardsPanelComponent.jsx
@@ -3,21 +3,43 @@ import { cloneDeep } from "lodash";
 import CardComponent from "./CardComponent";
 import SpinnerComponent from "./SpinnerComponent/SpinnerComponent";
 
-const dataArrInitial = [
-  { name: "sadfsdfasdf1", desc: "asdfrehserhsrgeargerherth1", id: 1 },
-  { name: "sadfsdfasdf2", desc: "asdfrehserhsrgeargerherth2", id: 2 },
-  { name: "sadfsdfasdf3", desc: "asdfrehserhsrgeargerherth3", id: 3 },
-  { name: "sadfsdfasdf4", desc: "asdfrehserhsrgeargerherth4", id: 4 },
-];
+// Example POST method implementation:
+async function getData(url = "") {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      "x-user-token":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQwOThmMzczZTE4ODgyM2FlY2Q5NDEiLCJiaXoiOnRydWUsImlhdCI6MTY0ODQwMjIyNH0.EepyQ0AJZ-qlA3qs55kdJc8ia5JAYeFKdp27r_FHNa0",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
 const CardsPanelComponent = () => {
   const [showSpinner, setShowSpinner] = useState(false);
-  const [dataArr, setDataArr] = useState(dataArrInitial);
+  const [dataArr, setDataArr] = useState([]);
 
   useEffect(() => {
     console.log("dataArr changed");
-    setShowSpinner(true);
+    setShowSpinner(false);
   }, [dataArr]);
+
+  useEffect(() => {
+    getData("http://localhost:3003/api/cards/myCards").then(
+      (dataFromServer) => {
+        console.log("dataFromServer", dataFromServer);
+        setDataArr(dataFromServer);
+      }
+    );
+  }, []);
 
   const handleDeleteCard = (id) => {
     setShowSpinner(true);
@@ -36,10 +58,10 @@ const CardsPanelComponent = () => {
       {dataArr.map((item) => {
         return (
           <CardComponent
-            name={item.name}
-            desc={item.desc}
-            id={item.id}
-            key={item.id}
+            name={item.bizName}
+            desc={item.bizDescription}
+            id={item._id}
+            key={item._id}
             onDeleteCard={handleDeleteCard}
           />
         );
